cf84bd24e3aa7ae31364904dcaf40c9edb952037
hyoaru <the.jadecabrera@gmail.com>
2026-08-14 12:33:17 +0800

    chore: add custom fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import "@fontsource-variable/dm-sans";
+import "@fontsource-variable/space-grotesk";
 import "./globals.css";
 
 const router = createRouter({ routeTree });
