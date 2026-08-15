9f6154b53415342af0471af51e625baa134a6c1c
hyoaru <the.jadecabrera@gmail.com>
2026-08-16 01:20:23 +0800

    chore: remove dependency context

--- src/infrastructure/router/routes/__root.tsx
+++ src/infrastructure/router/routes/__root.tsx
@@ -1,12 +1,9 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { DependenciesProvider } from "@/infrastructure/dependencies/context";
 
 export const Route = createRootRoute({
   component: () => (
-    <DependenciesProvider>
-      <div className="flex min-h-dvh items-start justify-start p-8">
-        <Outlet />
-      </div>
-    </DependenciesProvider>
+    <div className="flex min-h-dvh items-start justify-start p-8">
+      <Outlet />
+    </div>
   ),
 });
