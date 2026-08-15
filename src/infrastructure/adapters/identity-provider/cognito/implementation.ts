0a099fd2615c50d108d2577c12a36e2b0980132f
hyoaru <the.jadecabrera@gmail.com>
2026-08-16 01:54:52 +0800

    chore: add cognito authenticatin

--- src/infrastructure/adapters/identity-provider/cognito/implementation.ts
+++ src/infrastructure/adapters/identity-provider/cognito/implementation.ts
@@ -1,15 +1,51 @@
 import type { IdentityProvider } from "@/application/ports/identity-provider";
+import {
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+  SignUpCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 
 export class CognitoIdentityProvider implements IdentityProvider {
-  public async signUp(
-    _email: string,
-    _firstName: string,
-    _lastName: string,
-    _password: string,
-  ): Promise<void> {}
+  private cognitoClientId: string;
+  private cognitoClient: CognitoIdentityProviderClient;
 
-  public async signIn(
-    _email: string,
-    _password: string,
-  ): Promise<void> {}
+  public constructor() {
+    this.cognitoClientId = import.meta.env.VITE_AWS_COGNITO_CLIENT_ID;
+    this.cognitoClient = new CognitoIdentityProviderClient({
+      region: import.meta.env.VITE_AWS_REGION,
+    });
+  }
+
+  public async signUp(
+    email: string,
+    firstName: string,
+    lastName: string,
+    password: string,
+  ): Promise<void> {
+    await this.cognitoClient.send(
+      new SignUpCommand({
+        ClientId: this.cognitoClientId,
+        Username: email,
+        Password: password,
+        UserAttributes: [
+          { Name: "email", Value: email },
+          { Name: "custom:first_name", Value: firstName },
+          { Name: "custom:last_name", Value: lastName },
+        ],
+      }),
+    );
+  }
+
+  public async signIn(email: string, password: string): Promise<void> {
+    await this.cognitoClient.send(
+      new InitiateAuthCommand({
+        ClientId: this.cognitoClientId,
+        AuthFlow: "USER_PASSWORD_AUTH",
+        AuthParameters: {
+          USERNAME: email,
+          PASSWORD: password,
+        },
+      }),
+    );
+  }
 }
